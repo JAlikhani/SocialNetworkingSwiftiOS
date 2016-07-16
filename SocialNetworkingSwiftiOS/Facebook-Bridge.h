82c50b287111a1fe0e7d17ab7d408cde91998a3f
JAlikhani <alikhani.j2al@gmai.com>
2016-07-16 12:41:43 +1000

    Facebook login integrated

---
+++ SocialNetworkingSwiftiOS/Facebook-Bridge.h
@@ -0,0 +1,15 @@
+//
+//  Facebook-Bridge.h
+//  SocialNetworkingSwiftiOS
+//
+//  Created by Jalal Alikhani on 16/07/2016.
+//  Copyright © 2016 Jalal Alikhani. All rights reserved.
+//
+
+#ifndef Facebook_Bridge_h
+#define Facebook_Bridge_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Facebook_Bridge_h */
